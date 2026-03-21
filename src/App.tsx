76685fd85ff3bf30d57d7f036a497ac093a4732c
Havrylenko Olexii <hristinson@gmail.com>
2026-03-21 13:33:56 +0200

    First Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Lottie from "lottie-react";
 import redBallAnimation from "./animations/ball.json";
 
 export default function App() {
+
+  const [ballScale, setBallScale] = useState(0);
+  const updateScale = () => {
+    const { innerWidth } = window;
+    const scale = 2500 / innerWidth;
+    setBallScale(scale);
+  };
+
+  useEffect(() => {
+    updateScale();
+    window.addEventListener("resize", updateScale);
+    return () => window.removeEventListener("resize", updateScale);
+  }, []);
+
+
+  const scaledAnimation = {
+    ...redBallAnimation,
+    layers: redBallAnimation.layers.map((layer) => ({
+      ...layer,
+      ks: {
+        ...layer.ks,
+        s: { ...layer.ks.s, k: layer.ks.s.k.map((v) => v * ballScale) },
+      },
+      shapes: layer.shapes?.map((shape) => {
+        if (shape.ty === "el") {
+          return shape.s ? {
+            ...shape,
+            s: {
+              ...shape.s,
+              k: shape.s.k.map((v) => v * ballScale),
+            },
+          }  : {};
+        }
+        return shape;
+      }),
+    })),
+  };
+
   return (
     <div
       style={{
@@ -16,7 +54,7 @@ export default function App() {
       }}
     >
       <Lottie
-        animationData={redBallAnimation}
+        animationData={scaledAnimation}
         loop={true}
         autoplay={true}
         style={{
