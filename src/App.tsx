40be6527802ffd514bad2ff274ef3dee5898a30d
Havrylenko Olexii <hristinson@gmail.com>
2026-04-13 13:41:32 +0300

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-import Lottie from "lottie-react";
-import headAnimation from "./animations/head_new.json";
-import buttonAnimation from "./animations/buttonAnimation.json";
-import ScrollUpcontainer from "./scrollUpcontainer";
-import ScrollDowncontainer from "./scrollDowncontainer";
-import Schoolman from "./components/schoolman";
-import packageJson from "../package.json";
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import Contacts from "./pages/contacts";
 
 export default function App() {
   return (
-    <>
-      <div className="version">version {packageJson.version}</div>
-      <div className="appContainer">
-        <Schoolman />
-        <ScrollUpcontainer />
-        <div className="headWrapper">
-          <Lottie
-            animationData={headAnimation}
-            loop={true}
-            autoplay={true}
-            className="headMain"
-          />
-        </div>
-        <div className="buttonContainer">
-          <Lottie
-            onClick={() => {
-              alert("PRESS BUTTON");
-            }}
-            animationData={buttonAnimation}
-            loop={true}
-            autoplay={true}
-            className="button"
-          />
-        </div>
-        <ScrollDowncontainer />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/contacts" element={<Contacts />} />
+    </Routes>
   );
 }
---
+++ src/pages/contacts/index.tsx
@@ -0,0 +1,11 @@
+import ScrollDowncontainer from "../../scrollDowncontainer";
+
+export default function Contacts() {
+  return (
+    <div className="appContainer">
+      <div className="buttonContainer">Bla bla bla...</div>
+
+      <ScrollDowncontainer />
+    </div>
+  );
+}
