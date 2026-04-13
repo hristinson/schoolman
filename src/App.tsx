e7b3175db1013a0fcb8e8c6c90a2428e6837b36e
Havrylenko Olexii <hristinson@gmail.com>
2026-04-13 12:44:34 +0300

    Add Version control

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,39 @@ import buttonAnimation from "./animations/buttonAnimation.json";
 import ScrollUpcontainer from "./scrollUpcontainer";
 import ScrollDowncontainer from "./scrollDowncontainer";
 import Schoolman from "./components/schoolman";
+import packageJson from "../package.json";
 
 export default function App() {
   return (
-    <div className="appContainer">
-      <Schoolman />
-      <ScrollUpcontainer />
-      <div className="headWrapper">
-        <Lottie
-          animationData={headAnimation}
-          loop={true}
-          autoplay={true}
-          className="headMain"
-        />
+    <>
+      <div className="version">version {packageJson.version}</div>
+      <div className="appContainer">
+        <Schoolman />
+        <ScrollUpcontainer />
+        <div className="headWrapper">
+          <Lottie
+            animationData={headAnimation}
+            loop={true}
+            autoplay={true}
+            className="headMain"
+          />
+        </div>
+        <div className="buttonContainer">
+          <Lottie
+            onClick={() => {
+              alert("PRESS BUTTON");
+            }}
+            animationData={buttonAnimation}
+            loop={true}
+            autoplay={true}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </div>
+        <ScrollDowncontainer />
       </div>
-      <div className="buttonContainer">
-        <Lottie
-          onClick={() => {
-            alert("PRESS BUTTON");
-          }}
-          animationData={buttonAnimation}
-          loop={true}
-          autoplay={true}
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-        />
-      </div>
-      <ScrollDowncontainer />
-    </div>
+    </>
   );
 }
