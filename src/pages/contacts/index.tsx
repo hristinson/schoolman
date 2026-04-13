8fdf03e22a681ae4c96b4cdd9b35753400aeb296
Havrylenko Olexii <hristinson@gmail.com>
2026-04-13 14:17:48 +0300

    Add Social Networks

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,9 +1,20 @@
 import ScrollDowncontainer from "../../scrollDowncontainer";
+import { ReactComponent as Ximage } from "../../images/x.svg";
+import { ReactComponent as Instagram } from "../../images/instagram.svg";
+import { ReactComponent as Shareemail } from "../../images/shareemail.svg";
+import { ReactComponent as Sharefb } from "../../images/sharefacebook.svg";
+import { ReactComponent as Linkedin } from "../../images/linkedin.svg";
 
 export default function Contacts() {
   return (
     <div className="appContainer">
-      <div className="buttonContainer">Bla bla bla...</div>
+      <div className="linksContainer">
+        <Ximage />
+        <Instagram />
+        <Shareemail />
+        <Sharefb />
+        <Linkedin />
+      </div>
 
       <ScrollDowncontainer />
     </div>
