c9796446a22669e696db491a5a37a792b0012019
Samael <coolmound@gmail.com>
2026-05-02 21:48:43 +0300

    add program page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
 import Contacts from "./pages/contacts";
+import Prorgam from "./pages/program";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/contacts" element={<Contacts />} />
+      <Route path="/program" element={<Prorgam />} />
     </Routes>
   );
 }
